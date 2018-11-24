3bba71bc22676f69a1fc14226616b4af6a46ba9e
Nikolaos Papadakis <nickpapadakhs@gmail.com>
2018-11-24 14:05:54 +0200

    Project basis

---
+++ DSOB/Source/DSOB/DSOBGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DSOBGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DSOB_API ADSOBGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
